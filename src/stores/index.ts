12d3a75308a8bafaa5be07e67f5eb605c9ca19d6
maiguangzhi <1012737795@qq.com>
2023-02-21 18:52:54 +0800

    登录页面  打开网站Loading

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -4,6 +4,13 @@ import { defineStore } from 'pinia'
 // 定义Store实例并导出，useStore可以是任何东西，比如useUser, useCart
 // 第一个参数，唯一不可重复，字符串类型，作为仓库ID 以区分仓库
 // 第二个参数，以对象形式配置仓库的state,getters,actions
+// const store = useStore()
+// // 结合computed获取
+// const name = computed(() => store.name)
+// var i = 0;
+// // 解构并使数据具有响应式
+// const { age } = storeToRefs(store);
+// const handleClick = () => age++
 export const useStore = defineStore('main', {
   // state 推荐箭头函数，为了TS类型推断
   state: () => {
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,23 +1,48 @@
-import { useStore } from '@/stores/index'
-// 使普通数据变响应式的函数  
-import { storeToRefs } from 'pinia'
-import { ElButton } from 'element-plus'
+import { ElButton, ElInput, ElSpace } from 'element-plus'
+import initLoginBg from './init'
+import styles from './login.module.scss'
+import './login.less'
 
 export default defineComponent({
     setup() {
-        const store = useStore()
-        // 结合computed获取
-        const name = computed(() => store.name)
-        var i = 0;
-        // 解构并使数据具有响应式
-        const { age } = storeToRefs(store);
-        const handleClick = () => store.age++
+        const username = ref<string>();
+        const password = ref<string>();
+        const handleClick = () => {
+            console.log("username:", username, "password:", password);
+
+        }
+        onMounted(() => {
+            initLoginBg(); // 加载canvas背景
+            window.onresize = () => {
+                initLoginBg();// 窗口大小改变重新加载背景
+            };
+        })
         return () => (
-            <div>
-                <p>store.name:{store.name}</p>`
+            <div class={styles.loginContainer}>
+                {/* 存放背景 */}
+                <canvas id="canvas" class={styles.canvasBox}></canvas>
+
+                {/* 登录盒子 */}
+                <div class={styles.loginBox + " loginBox"}>
+                    {/* 标题部分 */}
+                    <div class={styles.title}>
+                        <h1>后台管理系统</h1>
+                        <p>Strive Everyday</p>
+                    </div>
+                    {/* 表单部分 */}
+                    <div class="form">
+                        <ElSpace direction="vertical" size="large" style={{ display: 'flex' }}>
+                            <ElInput v-model={username.value} placeholder="用户名"></ElInput>
+                            <ElInput v-model={password.value} placeholder="密码" type="password" show-password></ElInput>
+                            <ElButton type="primary" class="loginBtn" onClick={handleClick}>登录</ElButton>
+                        </ElSpace>
+                    </div>
+                </div>
+
+                {/* <p>store.name:{store.name}</p>`
                 <p>name:{name.value}</p>
                 <p>age:{age.value}</p>
-                <ElButton type={"primary"} onClick={handleClick}>点击</ElButton>
+                <ElButton type={"primary"} onClick={handleClick}>点击</ElButton> */}
             </div>
         )
     }
